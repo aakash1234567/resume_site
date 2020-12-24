acdb3f7db6ad168632ca08483a3203359597756e
aakash1234567 <aakashkhandelwal56@gmail.com>
2020-12-24 16:37:42 +0530

    live

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,11 +9,10 @@ const execa = require("execa");
       // Understand if it's dist or build folder
       const folderName = fs.existsSync("dist") ? "dist" : "build";
       await execa("git", ["--work-tree", folderName, "add", "--all"]);
-      await execa("git", ["--work-tree", folderName, "commit", "-m","Deploy"]);
       await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
       console.log("Pushing to gh-pages...");
       await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-      await execa("rm", ["-r", folderName]);
+      await execa("RD/S/Q", [folderName]);
       await execa("git", ["checkout", "-f", "main"]);
       await execa("git", ["branch", "-D", "gh-pages"]);
       console.log("Successfully deployed");
