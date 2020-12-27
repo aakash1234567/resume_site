b8091c97494050a660bb2d922714b64aeaae79a0
aakash1234567 <aakashkhandelwal56@gmail.com>
2020-12-27 20:43:51 +0530

    homepage

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,47 +1,33 @@
-/* tslint:disable:no-console */
+/* eslint-disable no-console */
 
-import { register } from 'register-service-worker';
+import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB',
-      );
-    },
-    registered() {
-      console.log('Service worker has been registered.');
+        'App is being served from cache by a service worker.\n'
+        + 'For more details, visit https://goo.gl/AFskqB',
+      )
     },
     cached() {
-      console.log('Content has been cached for offline use.');
+      console.log('Content has been cached for offline use.')
     },
     updatefound() {
-      console.log('New content is downloading.');
+      console.log('New content is downloading.')
     },
     updated() {
-      console.log('New content is available; please refresh.');
+      console.log('New content is available; please refresh.')
 
-      const script = document.createElement('script');
-      script.setAttribute('type', 'text/javascript');
-      script.innerHTML = 'window.location.reload(true);';
-
-      const meta = document.createElement('meta');
-      meta.setAttribute('http-equiv', 'refresh');
-      meta.setAttribute('content', '0;');
-
-      const noscript = document.createElement('noscript');
-      noscript.appendChild(meta);
-
-      const body = document.getElementsByTagName('body')[0];
-      body.appendChild(script);
-      body.appendChild(noscript);
+      setTimeout(() => {
+        window.location.reload(true)
+      }, 1000)
     },
     offline() {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.log('No internet connection found. App is running in offline mode.')
     },
     error(error) {
-      console.error('Error during service worker registration:', error);
+      console.error('Error during service worker registration:', error)
     },
-  });
+  })
 }
