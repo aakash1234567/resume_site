41193529e3d03889544a29a635aa7d9f77c2c84e
aakash1234567 <aakashkhandelwal56@gmail.com>
2020-12-27 20:36:06 +0530

    homepage

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+    },
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/resume_site/'
     : '/'
